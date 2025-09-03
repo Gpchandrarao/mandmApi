231bf308f8339387cc9783b7c1aa89a30440d1f2
Gpchandrarao <gandikotapoornachandrarao@gmail.com>
2025-09-03 18:15:01 +0530

    update workes id

--- models/Worker.js
+++ models/Worker.js
@@ -18,6 +18,7 @@ const clientSchema = new mongoose.Schema({
 
 const workerSchema = new mongoose.Schema(
   {
+    workerId: { type: Number, unique: true }, // 🔹 new numeric ID
     name: { type: String, required: true },
     number: { type: String, default: "" },
     role: { type: String, default: "Staff" }, // 👈 added
@@ -33,6 +34,18 @@ const workerSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Auto-increment workerId
+workerSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const lastWorker = await mongoose
+      .model("Worker")
+      .findOne()
+      .sort({ workerId: -1 });
+    this.workerId = lastWorker ? lastWorker.workerId + 1 : 1;
+  }
+  next();
+});
 
 const Worker = mongoose.model("Worker", workerSchema);
 export default Worker;
